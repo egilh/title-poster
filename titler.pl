00de97b697e4f77784b08236de159a3d30caeaf3
Egil Haugli <egil.haugli@visma.com>
2016-02-17 17:36:45 +0100

    changed position

--- titler.pl
+++ titler.pl
@@ -11,7 +11,6 @@ $VERSION = '0.1';
     description => 'Fetches and posts titles',
     license     => 'GNU General Public License 3.0' );
 
-
 my $chan = "#channel";
 my $titleline;
 my $titlestart;
@@ -20,42 +19,39 @@ my $word;
 my $url;
 my $urlfound = 0;
 my $title;
-my @denied_titles = ("Streamable - simple", "Jiffier gifs through HTML5 Video Conversion.",
-					"Untitled - Gfycat");
-
+my @denied_titles = (
+);
 
 sub get_title {
-    my ($server,$msg,$target, $channel, $chatnet) = @_;
+  my ($server,$msg,$target, $channel, $chatnet) = @_;
 
-    $_ = $msg;
-    if ($chatnet eq $chan) {
-      if (/^http.*.*$/ or /^https.*.*$/) {
-        my @splitline = split(' ',$_);
-        foreach $word (@splitline) {
-			if (index($word, "http") != -1) {
-					$urlfound = 1;
-					$url = $word;
-					last;
+  $_ = $msg;
+  if ($chatnet eq $chan) {
+    if (/^http.*.*$/ or /^https.*.*$/) {
+      my @splitline = split(' ',$_);
+      foreach $word (@splitline) {
+		    if (index($word, "http") != -1) {
+        $urlfound = 1;
+				$url = $word;
+				last;
 				}
 			}
       if ($urlfound eq 1) {
         $titleline = `wget -q -O- '$url' | awk "/<title>([^<]*)<\/"`;
         $titlestart = index($titleline, "<title>");
         $titleend = index($titleline, "</title>");
-        $title = substr $titleline, $titlestart + 7, -8;
+        $title = substr $titleline, $titlestart + 7, -9;
 				foreach my $d_title (@denied_titles) {
 					if (index($title, $d_title) ne -1) {
 						return;
 					}
 				}
-
 			if ($title ne "") {
 					$server->command("msg $chan $title");
 				}
 				$urlfound = 0;
 			}
-
 		}
-}
+  }
 }
 Irssi::signal_add('message public','get_title');
